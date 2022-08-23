47a4b5c5a1ec76466af06dbdf6abb93f626b51df
John Roller <jtroller33@gmail.com>
2022-08-23 18:28:57 -0500

    class

--- 8.23.22 MVC day 3 DONE/22 MVC Review/Unsolved/controllers/homeRoutes.js
+++ 8.23.22 MVC day 3 DONE/22 MVC Review/Unsolved/controllers/homeRoutes.js
@@ -1,8 +1,22 @@
 const router = require('express').Router();
+const { User } = require('../models');
 
 router.get('/', async (req, res) => {
-  // TODO: Render template with Sequelize data
-  res.render('homepage');
+  try {
+    // get all users sorted by name
+    const userData = await User.findAll({
+      attributes: { exclude: ['password'] },
+      order: [['name', 'ASC']],
+    });
+
+    // serialize user data so templates can read it
+    const users = userData.map((project) => project.get({ plain: true }));
+
+    // pass serialized data into handlebars template
+    res.render('homepage', { users });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
